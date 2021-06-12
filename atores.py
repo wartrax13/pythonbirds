3f97d959148b7fa75c5aafd038913c92c5247aac
Pedro <pedro.moisesandrade@gmail.com>
2021-06-12 18:02:31 -0300

    Teste de posição de lançamento.

--- atores.py
+++ atores.py
@@ -126,6 +126,7 @@ class Passaro(Ator):
         if self.foi_lancado():
             delta_t= tempo - self._tempo_de_lancamento
             self._calcular_posicao_vertical(delta_t)
+            self._calcular_posicao_horizontal(delta_t)
         return super().calcular_posicao(tempo)
 
 
@@ -148,6 +149,11 @@ class Passaro(Ator):
         y_atual -= (GRAVIDADE * (delta_t ** 2)) / 2
         self.y = y_atual
 
+    def _calcular_posicao_horizontal(self, delta_t):
+        x_atual=self._x_inicial
+        angulo_radianos = math.radians(self._angulo_de_lancamento)
+        x_atual += self.velocidade_escalar * delta_t * math.cos(angulo_radianos)
+        self.x=x_atual
 
 class PassaroAmarelo(Passaro):
     _caracter_ativo = 'A'
