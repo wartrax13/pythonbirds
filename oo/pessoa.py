2cc7f3a3c94477b654860ccbba7033ce31f008fe
Pedro <pedro.moisesandrade@gmail.com>
2021-06-12 17:05:32 -0300

    Realizado teste de atores.

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,7 +7,7 @@ class Pessoa:
         self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá, meu nome é {self.nome}'
 
     @staticmethod
     def metodo_estatico():
@@ -17,7 +17,9 @@ class Pessoa:
         return f'{cls} - olhos {cls.olhos}'
 
 class Homem(Pessoa):
-    pass
+    def cumprimentar(self):
+        cumprimentar_da_classe= super().cumprimentar()
+        return f'{cumprimentar_da_classe}. Aperto de mão'
 
 class Mutante(Pessoa):
     olhos = 3
@@ -50,3 +52,5 @@ if __name__ == '__main__':
     print(isinstance(renzo, Pessoa))  # verdadeiro: a pessoa acima é uma pessoa
     print(isinstance(renzo, Homem))  # falso: uma pessoa nao necessariamente é um Homem
     print(renzo.olhos)
+    print(pedro.cumprimentar())
+    print(renzo.cumprimentar())
